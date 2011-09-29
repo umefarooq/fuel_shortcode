c01509d94060f9f413bfd31e42827b3dff06c956
umefarooq <zerokool_007@yahoo.com>
2011-09-29 16:20:01 +0400

    first commit

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+Autoloader::add_classes(array(
+    'ShortCode\\Shortcode'        => __DIR__.'/classes/shortcode.php',
+    'ShortCode\\Codes'        => __DIR__.'/classes/codes.php',
+));
+
+?>
