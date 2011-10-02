8336d6fe3cee340282fc6917aba24bab92d5d954
Umer Farooq <zerokool_007@yahoo.com>
2011-10-02 11:48:03 +0400

    Edited bootstrap.php via GitHub

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,14 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * wordpress shortcode port to fuel now power up your cms with shortcode
+ * 
+ * @package     Fuel shortcode
+ * @author      umefarooq
+ * @license     MIT license
+ * @link        https://github.com/umefarooq/fuel_shortcode
  */
+
 
 Autoloader::add_classes(array(
     'ShortCode\\Shortcode'        => __DIR__.'/classes/shortcode.php',
